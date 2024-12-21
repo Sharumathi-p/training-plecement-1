864575c2d3cdb0df589cc09f7102a1b3f6ee323d
Sharumathi P <154426223+Sharumathi-p@users.noreply.github.com>
2024-12-21 18:12:44 +0530

    Create Remove Duplicates From Sorted Array.java

---
+++ 21.12.2024/Remove Duplicates From Sorted Array.java
@@ -0,0 +1,46 @@
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode();
+        ListNode current = dummy;
+        
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+        
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+        
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        
+        MergeTwoSortedLists merger = new MergeTwoSortedLists();
+        ListNode mergedList = merger.mergeTwoLists(list1, list2);
+        
+        while (mergedList != null) {
+            System.out.print(mergedList.val + " ");
+            mergedList = mergedList.next;
+        }
+    }
+}
