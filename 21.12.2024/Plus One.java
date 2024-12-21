e9f57134c6e2c5a3aee673f0da73690145b040c7
Sharumathi P <154426223+Sharumathi-p@users.noreply.github.com>
2024-12-21 18:49:55 +0530

    Create Plus One.java

---
+++ 21.12.2024/Plus One.java
@@ -0,0 +1,22 @@
+public class PlusOne {
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] newDigits = new int[digits.length + 1];
+        newDigits[0] = 1;
+        return newDigits;
+    }
+
+    public static void main(String[] args) {
+        int[] digits = {1, 2, 3};
+        int[] result = plusOne(digits);
+        for (int digit : result) {
+            System.out.print(digit + " ");
+        }
+    }
+}
